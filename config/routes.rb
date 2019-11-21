8451b0dcb4dc2e7eed6bca16d0066273893ebb1f
Brian Bower <brian@brianbower.com>
2019-11-21 09:57:15 -0700

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/merchants/find', to: "merchants#find"
-      get '/merchants/find_all', to: "merchants#find_all"
-      get '/merchants/random', to: "merchants#random"
+      get "/merchants/find", to: "merchants#find"
+      get "/merchants/find_all", to: "merchants#find_all"
+      get "/merchants/random", to: "merchants#random"
+      get "/merchants/most_revenue", to: "merchants#most_revenue"
+      get "/merchants/:id/items", to: "merchants/items#index"
+      get "/merchants/:id/invoices", to: "merchants/invoices#index"
       resources :merchants, only: [:index, :show]
+
+      resources :items, only: [:index]
     end
   end
 
