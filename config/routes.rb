0ccb673a71e69e6b4559074d5c622c6dacf92f2c
Brian Bower <brian@brianbower.com>
2019-11-21 13:12:29 -0700

    Add items routes and reorder merchants routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,18 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get "/merchants/find", to: "merchants#find"
-      get "/merchants/find_all", to: "merchants#find_all"
-      get "/merchants/random", to: "merchants#random"
-      get "/merchants/most_revenue", to: "merchants#most_revenue"
-      get "/merchants/:id/items", to: "merchants/items#index"
-      get "/merchants/:id/invoices", to: "merchants/invoices#index"
+      get '/merchants/random', to: 'merchants#random'
+      get '/merchants/find', to: 'merchants#find'
+      get '/merchants/find_all', to: 'merchants#find_all'
+      get '/merchants/most_revenue', to: 'merchants#most_revenue'
+      get '/merchants/:id/items', to: 'merchants/items#index'
+      get '/merchants/:id/invoices', to: 'merchants/invoices#index'
       resources :merchants, only: [:index, :show]
 
-      resources :items, only: [:index]
+      get '/items/random', to: 'items#random'
+      get '/items/find', to: 'items#find'
+      get '/items/find_all', to: 'items#find_all'
+      resources :items, only: [:index, :show]
     end
   end
 
