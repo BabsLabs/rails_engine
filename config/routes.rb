6fdfc65e564e6f540588a5049984cd30898689e3
Brian Bower <brian@brianbower.com>
2019-11-21 18:21:17 -0700

    Update route order

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+
+      get '/merchants/most_revenue', to: 'merchants#most_revenue'
       get '/merchants/random', to: 'merchants#random'
       get '/merchants/find', to: 'merchants#find'
       get '/merchants/find_all', to: 'merchants#find_all'
-      get '/merchants/most_revenue', to: 'merchants#most_revenue'
       get '/merchants/:id/items', to: 'merchants/items#index'
       get '/merchants/:id/invoices', to: 'merchants/invoices#index'
       resources :merchants, only: [:index, :show]
@@ -14,6 +15,7 @@ Rails.application.routes.draw do
       get '/items/find', to: 'items#find'
       get '/items/find_all', to: 'items#find_all'
       resources :items, only: [:index, :show]
+      
     end
   end
 
