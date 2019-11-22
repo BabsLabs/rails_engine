7eea11e4ec7ff4be85e8101761c60a9866c96413
Brian Bower <brian@brianbower.com>
2019-11-21 18:28:59 -0700

    Add query method for max_revenue_day for a specific date

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,9 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.max_revenue_day(date)
+    joins(:invoice_items, :transactions).where(transactions: {result: :success}).where(created_at: date.to_date.all_day ).sum('quantity * unit_price')
+  end
+
 end
