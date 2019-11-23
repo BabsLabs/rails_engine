b902e691e780f57a75871806f9496c67c43d20d9
Brian Bower <brian@brianbower.com>
2019-11-23 15:26:06 -0700

    Add case insensitiveness

--- db/migrate/20191119180654_create_items.rb
+++ db/migrate/20191119180654_create_items.rb
@@ -1,10 +1,14 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
+    enable_extension 'citext'
+
     create_table :items do |t|
       t.string :name
       t.string :description
       t.float :unit_price
       t.references :merchant, foreign_key: true
+      t.citext :name
+      t.citext :description
 
       t.timestamps
     end
