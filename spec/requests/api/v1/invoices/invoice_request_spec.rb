e5f621150beaf0b3f2848899298e3a58325ed31d
Brian Bower <brian@brianbower.com>
2019-11-23 21:55:21 -0700

    Add test for invoices show and specific tests for invoices index

--- spec/requests/api/v1/invoices/invoice_request_spec.rb
+++ spec/requests/api/v1/invoices/invoice_request_spec.rb
@@ -5,6 +5,7 @@ describe "Invoices API Endpoints" do
     merchant = create(:merchant)
     customer = create(:customer)
     create_list(:invoice, 10, merchant_id: merchant.id, customer_id: customer.id)
+    invoice = Invoice.first
 
     get '/api/v1/invoices'
 
@@ -14,6 +15,25 @@ describe "Invoices API Endpoints" do
     expect(invoices_check).to_not eq({"data"=>nil})
     expect(invoices_check['data'].count).to eq(10)
     expect(invoices_check['data'].first['type']).to eq('invoice')
+    expect(invoices_check['data'].first['id']).to eq(invoice.id.to_s)
+  end
+
+  it "can send a single invoices" do
+    merchant = create(:merchant)
+    customer = create(:customer)
+    create_list(:invoice, 10, merchant_id: merchant.id, customer_id: customer.id)
+    invoice_1 = Invoice.first
+    invoice_2 = Invoice.last
+
+    get "/api/v1/invoices/#{invoice_1.id}"
+
+    expect(response).to be_successful
+
+    invoices_check = JSON.parse(response.body)
+    expect(invoices_check).to_not eq({"data"=>nil})
+    expect(invoices_check['data']['type']).to eq('invoice')
+    expect(invoices_check['data']['id']).to eq(invoice_1.id.to_s)
+    expect(invoices_check['data'].values).not_to include(invoice_2.id.to_s)
   end
 
 end
