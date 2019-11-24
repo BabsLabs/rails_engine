227741d667b90683e43acebd8e2074ba61a5ba80
Brian Bower <brian@brianbower.com>
2019-11-23 21:33:19 -0700

    Add formatters to find actions for spec harness

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::Items::FindController < ApplicationController
 
   def index
-    items = Item.where("#{params.keys.first} = '#{params.values.first}'")
+    items = Item.where("#{params.keys.first} = '#{params.values.first}'").order(:id)
     serialized_items = ItemSerializer.new(items)
     render json: serialized_items
   end
 
   def show
-    items = Item.find_by("#{params.keys.first} = '#{params.values.first}'")
+    items = Item.where("#{params.keys.first} = '#{params.values.first}'").first
     serialized_items = ItemSerializer.new(items)
     render json: serialized_items
   end
