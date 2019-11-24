26505fe4546cf6822cd122a317411c492c209d92
Brian Bower <brian@brianbower.com>
2019-11-24 10:13:10 -0700

    Add index action and update show action to find by query params

--- app/controllers/api/v1/invoices/find_controller.rb
+++ app/controllers/api/v1/invoices/find_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::Invoices::FindController < ApplicationController
 
+  def index
+    invoices = Invoice.where("#{params.keys.first} = '#{params.values.first}'").order(:id)
+    serialized_invoices = InvoiceSerializer.new(invoices)
+    render json: serialized_invoices
+  end
+
   def show
-    invoice = Invoice.find(params[:id])
+    invoice = Invoice.find_by("#{params.keys.first} = '#{params.values.first}'")
     serialized_invoice = InvoiceSerializer.new(invoice)
     render json: serialized_invoice
   end
