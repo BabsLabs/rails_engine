8ef214a94abd5a50c6a954b221c800606e9c7736
Brian Bower <brian@brianbower.com>
2019-11-24 16:49:20 -0700

    Add routes for invoice transactions, invoice invoice_items, and invoice items

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,9 @@ Rails.application.routes.draw do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
         get '/random', to: 'random#show'
+        get '/:id/transactions', to: 'transactions#index'
+        get ':id/invoice_items', to: 'invoice_items#index'
+        get ':id/items', to: 'items#index'
       end
 
       resources :invoices, only: [:index, :show]
