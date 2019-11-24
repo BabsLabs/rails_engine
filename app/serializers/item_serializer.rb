750da5aa4b66a3c68679bfc9110e9746af8fa3e6
Brian Bower <brian@brianbower.com>
2019-11-23 21:32:47 -0700

    Add merchant_id

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :name, :description, :unit_price
+  attributes :id, :name, :description, :unit_price, :merchant_id
 end
