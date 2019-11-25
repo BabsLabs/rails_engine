98da8e5c5cf6f23a2465b2b68994d5263f0f1ee5
Brian Bower <brian@brianbower.com>
2019-11-24 18:55:09 -0700

    Add faker for name and description

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyString" }
+    name { Faker::Commerce.unique.product_name }
+    description {Faker::Lorem.sentence(word_count: 5, random_words_to_add: 4) }
     unit_price { 1.5 }
     merchant { nil }
   end
