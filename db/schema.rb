ac916c4309a42f28b4501b36cb3ce7e370cfb5b6
Brian Bower <brian@brianbower.com>
2019-11-25 13:51:38 -0700

    Change unit_price from float to string for spec_harness tests

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_185505) do
     t.bigint "item_id"
     t.bigint "invoice_id"
     t.integer "quantity"
-    t.float "unit_price"
+    t.string "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
