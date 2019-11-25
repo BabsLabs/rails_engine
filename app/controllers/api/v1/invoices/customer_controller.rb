24a26bf2ba79a7c2ef4d9fb5b4ca9e1ea3b8cd64
Brian Bower <brian@brianbower.com>
2019-11-24 17:48:20 -0700

    Make serializer singular instead of plural

--- app/controllers/api/v1/invoices/customer_controller.rb
+++ app/controllers/api/v1/invoices/customer_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::Invoices::CustomerController < ApplicationController
 
-  def index
+  def show
     invoice = Invoice.find(params[:id])
-    serialzed_invoice = InvoiceCustomersSerializer.new(invoice)
+    serialzed_invoice = InvoiceCustomerSerializer.new(invoice)
     render json: serialzed_invoice
   end
 
--- app/serializers/invoice_customer_serializer.rb
+++ app/serializers/invoice_customer_serializer.rb
@@ -1,4 +1,4 @@
-class InvoiceCustomersSerializer
+class InvoiceCustomerSerializer
   include FastJsonapi::ObjectSerializer
 
     attributes :id, :customer_id, :merchant_id, :status
