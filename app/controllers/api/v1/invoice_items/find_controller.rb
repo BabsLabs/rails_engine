2132b4320d3288a21cb4ad6f99719bb12a95ee83
Brian Bower <brian@brianbower.com>
2019-11-25 13:50:13 -0700

    Add order to index AR action

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::InvoiceItems::FindController < ApplicationController
 
   def index
-    invoice = InvoiceItem.where("#{params.keys.first} = '#{params.values.first}'")
+    invoice = InvoiceItem.where("#{params.keys.first} = '#{params.values.first}'").order(:id)
     serializerd_inovice_item = InvoiceItemSerializer.new(invoice)
     render json: serializerd_inovice_item
   end
