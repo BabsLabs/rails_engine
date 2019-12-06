ae79e6020ab9e1f0de8c020a60660ecb10db3db0
Brian Bower <brian@brianbower.com>
2019-12-06 13:56:01 -0700

    Update to private find params

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -1,15 +1,21 @@
 class Api::V1::Customers::FindController < ApplicationController
 
   def index
-    customers = Customer.where("#{params.keys.first} = '#{params.values.first}'")
+    customers = Customer.where(customers_params)
     serialized_customers = CustomerSerializer.new(customers)
     render json: serialized_customers
   end
 
   def show
-    customer = Customer.find_by("#{params.keys.first} = '#{params.values.first}'")
+    customer = Customer.find_by(customers_params)
     serialized_customer = CustomerSerializer.new(customer)
     render json: serialized_customer
+  end
+
+  private
+
+  def customers_params
+    params.permit(:id, :first_name, :last_name, :created_at, :updated_at)
   end
 
 end
